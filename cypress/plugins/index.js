9e17594e6c114499398a5f94f015000c04569d5f
Ribake Ikumoluyi <reiybake05@googlemail.com>
2019-07-11 13:03:17 +0100

    Adding config for maximising runner and browser upon launch

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -8,23 +8,33 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+const cucumber = require('cypress-cucumber-preprocessor').default
+
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing). RIBAKE: This is where we define events that 
 // Cypress should fire by itself during the time that it is running, and the handlers
 // for such events 
+
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
+
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 
   // Fire the before:browser:launch event. The handler maximises the browser
   on('before:browser:launch', (browser = {}, args) => {
-    if(browser.name === 'electron' || browser.name === 'chrome') {
+    // For electron browser, set up and return the args variable that the runner would use
+    if (browser.name === 'electron') {
+      // args.push('--start-fullscreen')
+      args['fullscreen'] = true
+      return args
+    }
+
+    // Because chrome is going to be used with the Cypress command line, args would be available and we can push to it 
+    if(browser.name === 'chrome') {
       args.push('--start-fullscreen')
     }
   })
 }
-// module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-// }
-
-const cucumber = require('cypress-cucumber-preprocessor').default
